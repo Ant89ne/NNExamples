27c7b1f265d24b661cc2d16dcd5a496baadd5a74
Ant89ne <antoine.bralet@cpe.fr>
2021-12-04 19:11:22 +0100

    bug correction, visualization OK, NN TBD

--- Classif2D.py
+++ Classif2D.py
@@ -17,25 +17,30 @@ class myNet(nn.Module):
 
 
 class myDataset(Dataset):
-    def __init__(self):
-        self.length = 1000
-        self.a = 0.5
-        self.b = 0.2
-        self.seed = 25
+    def __init__(self):         #Function TO BE filled
+        #Initialization
+        self.length = 1000  #Number of points in the dataset  
+        self.a = 0.5        #Direction of the line
+        self.b = 0.2        #Biais of the line
+        self.seed = 25      #Seed (for random generation)
         random.seed(self.seed)
 
-    def __len__(self):
+    def __len__(self):          #Function TO BE filled
+        #Return the length of the dataset
         return self.length
         
-    def __getitem__(self, index):
-        x1 = random.random()
+    def __getitem__(self, index): #Function TO BE filled
+        #Return the data at the given index
+        x1 = random.random()    #Creation of the coordinates
         x2 = random.random()
 
+        #Creation of the label (0 if under the line, 1 if above)
         if x2 < (self.a*x1+self.b) :
             l = torch.Tensor(np.array([1,0]))
         else : 
             l = torch.Tensor(np.array([0,1]))
         
+        #Convert from array to Tensor (for training purpose)
         X = torch.Tensor(np.array([x1,x2]))
         return X,l
 
@@ -43,13 +48,15 @@ class myDataset(Dataset):
 #Initialization
 mydata = myDataset()        #Dataset
 
+#Dataset visualization
 plt.figure(0)
-X = np.array([np.array(next(iter(mydata))[0]) for i in range(len(mydata)) ])
-L = np.array([np.array(next(iter(mydata))[1]) for i in range(len(mydata)) ])
-verif = X[0:10,:]
-verif[:,0] = verif[:,0] * 0.5 + 0.2
-print(verif, L[0:10,:])
-
+#Get data
+X = np.array([np.array(next(iter(mydata))) for i in range(len(mydata)) ])
+#Extract labels (y)
+L = np.array([np.array(x) for x in X[:,1]])
+#Extracts coordinates (x)
+X = np.array([np.array(x) for x in X[:,0]])
+#Group data by labels
 X1 = []
 X0 = []
 for i,k in enumerate(L) :
@@ -57,20 +64,16 @@ for i,k in enumerate(L) :
         X0.append(X[i,:])
     elif k[1] == 1 :
         X1.append(X[i,:])
-
-
-
 X0 = np.array(X0)
 X1 = np.array(X1)
-
-verif = X0[0:10,:]
-verif[:,0] = verif[:,0] * 0.5 + 0.2
-print(verif)
+#Visualize and save
 plt.plot(X0[:,0], X0[:,1], 'r*')
 plt.plot(X1[:,0], X1[:,1], 'b*')
 plt.savefig("pilou")
 
-exit()
+
+
+
 model = myNet()             #Neural Net
 
 mydataloader = DataLoader(mydata, 8, shuffle = True)    #Dataloader
@@ -89,7 +92,7 @@ for e in range(epoch):
         #Initialization of the gradient error
         optimizer.zero_grad()
 
-        #Prediction of the labels from the input data by the net
+        #Prediction of the labels from the input data by the net (y^)
         pred = model(data)
 
         #Calculation of the loss
